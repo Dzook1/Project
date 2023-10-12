73a9db21a82b445165573c9d8b4d27c1e1e22a58
Dzook <dzook099@gmail.com>
2023-10-12 10:18:44 -0400

    update

--- home.js
+++ home.js
@@ -2,9 +2,12 @@ function display(x){
     let display = document.getElementById('display');
     display.innerHTML += x.innerHTML;
 }
+
 function solve(){
     let x = document.getElementById('display').innerHTML
     let y = eval(x);
     document.getElementById('display').innerHTML = y
     return y
 }
+
+
